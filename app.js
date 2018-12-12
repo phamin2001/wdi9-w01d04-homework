1579c708a8d86da372ca994d31984ff9d4070041
phamin2001 <amin.pahlavani@gmail.com>
2018-12-12 16:26:47 -0700

    Commit 1 - Verbal questions.

---
+++ app.js
@@ -0,0 +1,9 @@
+// Homework - W01d04
+
+// 1. Verbal questions
+// 1: Parameters are the names listed in the function definition. Arguments are the actual values that 
+// passed into the functions when the functions is called. 
+// 2. with console.log we are printing an output of the function 
+// but with return, function returning a result of the function to where it was called.
+// 3. we could use that reurn value as much as we want in diffrent part of our program 
+
